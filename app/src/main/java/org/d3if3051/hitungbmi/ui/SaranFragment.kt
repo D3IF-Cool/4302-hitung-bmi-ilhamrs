4c0b2d6741a2bc85df8721f6243039ee988f9ae1
ilham_ramadhani_saputro <dkijakartaramasaputro@gmail.com>
2021-03-18 15:59:55 +0700

    Mengirim data saat bernavigasi

--- app/src/main/java/org/d3if3051/hitungbmi/ui/SaranFragment.kt
+++ app/src/main/java/org/d3if3051/hitungbmi/ui/SaranFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import org.d3if3051.hitungbmi.R
 import org.d3if3051.hitungbmi.data.KategoriBmi
 import org.d3if3051.hitungbmi.databinding.FragmentSaranBinding
 
+
 class SaranFragment : Fragment() {
+    private val args: SaranFragmentArgs by navArgs()
     private lateinit var binding: FragmentSaranBinding
 
     override fun onCreateView(
@@ -19,7 +22,7 @@ class SaranFragment : Fragment() {
         savedInstanceState: Bundle?): View?{
         binding = FragmentSaranBinding.inflate(
             layoutInflater, container, false)
-        updateUI(KategoriBmi.KURUS)
+        updateUI(args.kategori)
         return binding.root
     }
 
